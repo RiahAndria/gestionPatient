2da5ee2c8d35ef8dc438099b7abd8bd826755262
RiahAndria <gypsygypsy324@gmail.com>
2026-07-25 23:49:33 +0300

    Ajout sidebar, ajout bg, finition page patient

---
+++ Views/Components/SidebarView.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows.Controls;
+
+namespace Patients.Views.Components;
+
+public partial class SidebarView : UserControl
+{
+    public SidebarView()
+    {
+        InitializeComponent();
+    }
+
+    // Expose les RadioButtons pour le Binding ou l'interaction directe depuis MainWindow
+    public RadioButton PatientsButton => NavPatients;
+    public RadioButton MedecinsButton => NavMedecins;
+    public RadioButton DossiersButton => NavDossiers;
+    public RadioButton HistoriqueButton => NavHistorique;
+    public RadioButton RendezVousButton => NavRendezVous;
+}
